08388bd20aefe1c8afb31fcd1aae2fc955320c0d
jnnkim <joannekim806@gmail.com>
2024-11-22 12:47:07 -0700

    Added comments and more tile funct.

--- Tile.cpp
+++ Tile.cpp
@@ -2,33 +2,40 @@
 
 #include "Tile.h"
 
-    // void Tile::Green(_player){
-    //     //random events (50% chance of triggering)
-    // }
+    void Tile::Green(Player _player){
+        /*
+        1. Determine whether to trigger a random event (50% chance of triggering)
+        2. If triggered, choose from a random event (riddles, challenges, etc.)
+        3. Run the event and update player skill points based on events
+        */
+    }
 
     void Tile::Pink(Player _player){
         _player.addStamina(300);
         _player.addStrength(300);
         _player.addWisdom(300);
-        //also able to choose your advisor
+        // 1. The player gets an option to choose an advisor
+        // 2. If the player already has an advisor, allow them to switch to a different one if wanted
     }
 
-    // void Tile::Red(){
-    //     _player.addStamina(-100);
-    //     _player.addWisdom(-100);
-    //     _player.addStrength(-100);
-    //     //move back 10 tiles
-    // }
+    void Tile::Red(Player _player){
+        _player.addStamina(-100);
+        _player.addWisdom(-100);
+        _player.addStrength(-100);
+        // 1. Also implement moving the player back 10 tiles
+    }
 
-    // void Tile::Blue(){
-    //     _player.addStamina(200);
-    //     _player.addWisdom(200);
-    //     _player.addStrength(200);
-    // }
+    void Tile::Blue(Player _player){
+        _player.addStamina(200);
+        _player.addWisdom(200);
+        _player.addStrength(200);
+    }
 
-    char Tile::determineColor(char tileColors[]){
-        for (int i = 0; i < 52; i++){
-            cout << tileColors[i] << endl;
-        }
-        return 'H';
+    void Brown(Player _player);
+    void Purple(Player _player);
+    void Orange(Player _player);
+    void Grey(Player _player);
+
+    char Tile::determineColor(){
+        // 1. Based on position of the player, determine the color of the tile that the player is on
     }
