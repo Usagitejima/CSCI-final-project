59db87e73dd13c84f723bce6288528fab1b33368
Usagi Tejima <dolgormaadonna@gmail.com>
2024-11-26 16:17:04 -0700

    Made a board generate random boards for each traning and pride path. Board 1 is pride land which has no pink tiles. Board 2 is cub training which has no red tiles.

--- Board.h
+++ Board.h
@@ -11,19 +11,23 @@ class Board
 private:
     static const int _BOARD_SIZE = 52;
     Tile _tiles[2][_BOARD_SIZE];
-    static const int _MAX_PLAYERS = 2;
+    static const int _MAX_PLAYERS = 4;
     int _player_count;
     int _player_position[_MAX_PLAYERS];
     void displayTile(int player_index, int pos);
-    void initializeTiles(int player_index);
+    void initializePrideTiles(int player_index);
+    void initializeTrainTiles(int player_index);
     bool isPlayerOnTile(int player_index, int pos);
 
 public:
     Board();
     Board(int player_count);
-    void displayTrack(int player_index);
-    void initializeBoard();
-    void displayBoard();
+    void displayPrideTrack(int player_index);
+    void displayTrainTrack(int player_index);
+    void initializePrideBoard();
+    void initializeTrainBoard();
+    void displayPrideBoard();
+    void displayTrainBoard();
     bool movePlayer(int player_index);
     int getPlayerPosition(int player_index) const;
 
